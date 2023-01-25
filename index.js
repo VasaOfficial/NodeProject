1ee605b46027f50bf9e9ad229ce7c726c4da1b86
VasaOfficial <VasaPofficial@gmail.com>
2023-01-25 21:25:18 +0100

    refactore the code with express

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
-const http = require("http");
+const express = require("express");
+const app = express();
 const fs = require("fs");
 const port = 8080;
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    fs.readFile("./index.html", (err, data) => {
-      if (err) throw err;
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-    });
-  } else if (req.url === "/about") {
-    fs.readFile("./about.html", (err, data) => {
-      if (err) throw err;
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-    });
-  } else if (req.url === "/contact-me") {
-    fs.readFile("./contact-me.html", (err, data) => {
-      if (err) throw err;
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-    });
-  } else {
-    fs.readFile("./404.html", (err, data) => {
-      if (err) throw err;
-      res.writeHead(404, { "Content-Type": "text/html" });
-      res.end(data);
-    });
-  }
+app.get("/", (req, res) => {
+  fs.readFile("./index.html", (err, data) => {
+    if (err) throw err;
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+  });
 });
 
-server.listen(port, () => {
+app.get("/about", (req, res) => {
+  fs.readFile("./about.html", (err, data) => {
+    if (err) throw err;
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+  });
+});
+
+app.get("/contact-me", (req, res) => {
+  fs.readFile("./contact-me.html", (err, data) => {
+    if (err) throw err;
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+  });
+});
+
+app.use((req, res) => {
+  fs.readFile("./404.html", (err, data) => {
+    if (err) throw err;
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end(data);
+  });
+});
+
+app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
